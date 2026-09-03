24ca15fbc657ed504337e3ece23048965a84c491
Pavel Miasnov <glowfall@gmail.com>
2026-09-03 10:55:14 +0100

    Remove TLS handling from application

--- main.go
+++ main.go
@@ -11,8 +11,7 @@ import (
 	"github.com/glowfall/asot/asot"
 )
 
-var useTLS = flag.Bool("useTLS", true, "--useTLS=false")
-var listenAddress = flag.String("listenAddress", ":80", "HTTP listen address when TLS is disabled")
+var listenAddress = flag.String("listenAddress", "127.0.0.1:8080", "HTTP listen address")
 
 //go:embed static
 var staticDir embed.FS
@@ -22,30 +21,9 @@ func main() {
 
 	mux := setupMux()
 
-	if *useTLS {
-		go func() {
-			log.Printf("Listening on :80 for redirects\n")
-			if err := http.ListenAndServe(":80", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-				targetUrl := url.URL{
-					Scheme:   "https",
-					Host:     r.Host,
-					Path:     r.URL.Path,
-					RawQuery: r.URL.RawQuery,
-				}
-				http.Redirect(rw, r, targetUrl.String(), http.StatusMovedPermanently)
-			})); err != nil {
-				log.Fatalf("https redirect ListenAndServe error: %+v", err)
-			}
-		}()
-		log.Printf("Listening on :443\n")
-		if err := http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/mias.top/cert.pem", "/etc/letsencrypt/live/mias.top/privkey.pem", mux); err != nil {
-			log.Fatalf("ListenAndServe error: %+v", err)
-		}
-	} else {
-		log.Printf("Listening on http://%s/\n", *listenAddress)
-		if err := http.ListenAndServe(*listenAddress, mux); err != nil {
-			log.Fatalf("ListenAndServe error: %+v", err)
-		}
+	log.Printf("Listening on http://%s/\n", *listenAddress)
+	if err := http.ListenAndServe(*listenAddress, mux); err != nil {
+		log.Fatalf("ListenAndServe error: %+v", err)
 	}
 }
 
